ef6e0f75c616a67fc21649d4897daa1f4b9babb0
Rakshan Thapar <rakshanthapar32@gmail.com>
2023-09-05 22:53:20 +0530

    Using Variable instead of hardCoding Data

--- budgettracker/src/Components/ExpenseItem.js
+++ budgettracker/src/Components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+  const expenseDate = new Date(2023, 8, 5);
+  const expenseTitle = "Food";
+  const expenseAmount = 190;
+
   return (
     <div className="expense-item">
-      <div>March 28th 2021</div>
+      <div>{expenseDate.toDateString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">Rs {expenseAmount}</div>
       </div>
     </div>
   );
